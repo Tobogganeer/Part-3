3e4a9a11758311070dc4bcc5bcffa5a699460e46
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-03-13 15:33:38 -0400

    Task 4b complete: display current character type on-screen

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TMPro.TMP_Text selectedCharacterText;
+
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -15,5 +17,11 @@ public class CharacterControl : MonoBehaviour
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
     }
-    
+
+    // Might not be the most efficient to do it in update, but it's easy
+    private void Update()
+    {
+        // Show the current character's type, if there is a current character
+        selectedCharacterText.text = SelectedVillager ? SelectedVillager.GetChestType().ToString() : "No Character";
+    }
 }
