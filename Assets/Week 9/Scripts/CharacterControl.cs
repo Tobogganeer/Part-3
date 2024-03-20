bd2fb6a0aa67be8950ef8140d5b03d601e79db19
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-03-20 15:07:07 -0400

    Task 4b complete: scaling villagers from slider

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -13,6 +13,7 @@ public class CharacterControl : MonoBehaviour
 
 
     public TMPro.TMP_Text selectedCharacterText;
+    public UnityEngine.UI.Slider scaleSlider;
     public Villager[] villagers;
 
     public static Villager SelectedVillager { get; private set; }
@@ -23,7 +24,11 @@ public class CharacterControl : MonoBehaviour
 
         SelectedVillager = villager;
         if (SelectedVillager != null)
+        {
             SelectedVillager.Selected(true);
+            // Set the slider to match our current villager
+            instance.scaleSlider.value = SelectedVillager.scale;
+        }
 
         // Show the current character's type (if we have one selected)
         instance.selectedCharacterText.text = SelectedVillager == null ? "No Character" : SelectedVillager.ToString();
@@ -43,5 +48,13 @@ public class CharacterControl : MonoBehaviour
             SetSelectedVillager(null);
         else
             SetSelectedVillager(villagers[index]);
+    }
+
+    public void SetCurrentVillagerScale(float scale)
+    {
+        // Should be clamped from the slider but we'll make sure it is
+        scale = Mathf.Clamp(scale, 0.5f, 2f);
+        if (SelectedVillager != null)
+            SelectedVillager.scale = scale;
     }
 }
