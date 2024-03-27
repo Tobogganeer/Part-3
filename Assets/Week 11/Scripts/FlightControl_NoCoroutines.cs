bb363889969877714f6e9003ddeece04510bc04d
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-03-27 14:40:22 -0400

    Task 4: Converted code to update

--- Assets/Week 11/Scripts/FlightControl_NoCoroutines.cs
+++ Assets/Week 11/Scripts/FlightControl_NoCoroutines.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,48 +8,57 @@ public class FlightControl_NoCoroutines : MonoBehaviour
     public GameObject missile;
     public float speed = 5;
     public float turningSpeedReduction = 0.75f;
-    Coroutine coroutine;
+
+    Action currentAction;
+    Quaternion startHeading;
+    Quaternion targetHeading;
+    float interpolation;
+
+    float legLength;
+    float time;
+
+    private void Update()
+    {
+        currentAction?.Invoke();
+    }
 
     public void MakeTurn(float turn)
     {
-        if (coroutine != null)
-        {
-            StopCoroutine(coroutine);
-        }
-        coroutine = StartCoroutine(Turn(turn));
+        // Initialize the turn values
+        interpolation = 0;
+        startHeading = missile.transform.rotation;
+        targetHeading = startHeading * Quaternion.Euler(0, 0, turn);
+
+        // Make update tick our Turn function (could use booleans or enums or whatever but oh well)
+        currentAction = Turn;
     }
 
     public void MoveForwards(float length)
     {
-        if (coroutine != null)
-        {
-            StopCoroutine(coroutine);
-        }
-        coroutine = StartCoroutine(RunLeg(length));
+        // Init the movement
+        legLength = length;
+        time = 0;
+
+        // Tick the movement update function
+        currentAction = RunLeg;
     }
 
-    IEnumerator RunLeg(float legLength)
+    void RunLeg()
     {
-        float time = 0;
-        while (time < legLength)
+        if (time < legLength)
         {
             time += Time.deltaTime;
             missile.transform.Translate(transform.right * speed * Time.deltaTime);
-            yield return null;
         }
     }
 
-    IEnumerator Turn(float turn)
+    void Turn()
     {
-        float interpolation = 0;
-        Quaternion currentHeading = missile.transform.rotation;
-        Quaternion newHeading = currentHeading * Quaternion.Euler(0, 0, turn);
-        while (interpolation < 1)
+        if (interpolation < 1)
         {
             interpolation += Time.deltaTime;
-            missile.transform.rotation = Quaternion.Lerp(currentHeading, newHeading, interpolation);
+            missile.transform.rotation = Quaternion.Lerp(startHeading, targetHeading, interpolation);
             missile.transform.Translate(transform.right * (speed * turningSpeedReduction) * Time.deltaTime);
-            yield return null;
         }
     }
 }
