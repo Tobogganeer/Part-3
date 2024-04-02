c0da28c268a8df587b897d5f4d451495718555ad
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-04-01 23:42:12 -0400

    Added extra little button scripts

---
+++ Assets/Assignment/Scripts/ToolbarButton.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolbarButton : MonoBehaviour
+{
+    // Basically store a ref to the toolbar, store a building type, tell it when we are pressed
+    // Simple life...
+}
