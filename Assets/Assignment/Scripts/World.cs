5f7f5b72ca7b3dbe102bd90508bfb877834bd911
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-04-02 02:10:49 -0400

    World gizmos and centered spawning

--- Assets/Assignment/Scripts/World.cs
+++ Assets/Assignment/Scripts/World.cs
@@ -13,6 +13,7 @@ public class World : MonoBehaviour
     public GameObject worldTilePrefab;
     public float worldTileSize; // Units
     public Vector2Int worldSize; // Tiles
+    public Vector2 worldPositionOffset; // Units
     public SerializableDictionary<Vector2Int, ProductID> resourceLocations;
 
     // All tiles in the world
@@ -40,15 +41,25 @@ public class World : MonoBehaviour
         }
     }
 
-    void SpawnTile(Vector2Int position, ProductID product, Transform holder)
+    void SpawnTile(Vector2Int gridPosition, ProductID product, Transform holder)
     {
         // Convert the grid coordinate to actual world space
-        Vector2 worldPosition = (Vector2)position * worldTileSize + (Vector2)transform.position;
-        GameObject spawnedTile = Instantiate(worldTilePrefab, worldPosition, Quaternion.identity, holder);
+        Vector2 tileWorldPosition = (Vector2)gridPosition * worldTileSize + GetWorldOffset();
+        // Spawn and scale the tile
+        GameObject spawnedTile = Instantiate(worldTilePrefab, tileWorldPosition, Quaternion.identity, holder);
+        spawnedTile.transform.localScale = Vector3.one * worldTileSize;
         // Tell the tile what and where it is
         WorldTile tile = spawnedTile.GetComponent<WorldTile>();
-        tile.Init(product, position);
-        worldTiles.Add(position, tile);
+        tile.Init(product, gridPosition);
+        worldTiles.Add(gridPosition, tile);
+    }
+
+    Vector2 GetWorldOffset()
+    {
+        // Center the world and then add the world offset
+        Vector2 halfWorldSize = (Vector2)worldSize / 2f * worldTileSize;
+        Vector2 halfTileSize = Vector2.one * worldTileSize / 2f;
+        return worldPositionOffset - halfWorldSize + halfTileSize;
     }
 
     public static bool CanPlaceBuilding(Vector2Int gridPosition, FactoryBuilding building)
@@ -68,6 +79,37 @@ public class World : MonoBehaviour
         // TODO: Remove building and tiles
         throw new System.NotImplementedException();
     }
+
+
+    private void OnDrawGizmos()
+    {
+        // Code essentially stolen from Start
+
+        for (int x = 0; x < worldSize.x; x++)
+        {
+            for (int y = 0; y < worldSize.y; y++)
+            {
+                Vector2Int position = new Vector2Int(x, y);
+                // Did we specify there being a resource to mine here?
+                bool actualResourceHere = resourceLocations.dict.ContainsKey(position);
+                // Fetch the resource (if it exists)
+                ProductID product = actualResourceHere ? resourceLocations.dict[position] : ProductID.None;
+
+                Gizmos.color = GetProductColour(product);
+                Vector2 tileWorldPosition = (Vector2)position * worldTileSize + GetWorldOffset();
+                Gizmos.DrawWireCube(tileWorldPosition, Vector2.one * (worldTileSize * 0.95f));
+            }
+        }
+    }
+
+    Color GetProductColour(ProductID product) => product switch
+    {
+        ProductID.Water => Color.blue,
+        ProductID.Wheat => Color.yellow,
+        ProductID.Tomato => Color.red,
+        ProductID.Can => Color.grey,
+        _ => Color.green // Default colour
+    };
 }
 
 /*
