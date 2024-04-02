94ad498cd3bcda8ee5eb4610c1f04cd7982cb2dc
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-04-02 02:14:52 -0400

    Fix for SerializableDictionary not updating

--- Assets/Assignment/Scripts/Util.cs
+++ Assets/Assignment/Scripts/Util.cs
@@ -27,21 +27,34 @@ public class SerializableDictionary<TKey, TValue>
     [SerializeField] SerializedPair[] _values;
 
     Dictionary<TKey, TValue> _dict;
+    /// <summary>
+    /// A copy of the inspector's values.
+    /// </summary>
+    /// <remarks>Only use if the values will not change. If in Gizmos or similar, use <seealso cref="CreateDict"/></remarks>
     public Dictionary<TKey, TValue> dict
     {
         get
         {
-            if (_dict == null)
+            if (_dict == null || _dict.Count != _values.Length)
             {
                 // Add the values from the inspector array to the dict
-                _dict = new Dictionary<TKey, TValue>();
-                foreach (SerializedPair pair in _values)
-                {
-                    _dict.Add(pair.key, pair.value);
-                }
+                _dict = CreateDict();
             }
 
             return _dict;
         }
+    }
+
+    /// <summary>
+    /// Creates a fresh copy of the inspector's values.
+    /// </summary>
+    /// <returns></returns>
+    public Dictionary<TKey, TValue> CreateDict()
+    {
+        Dictionary<TKey, TValue> newDict = new Dictionary<TKey, TValue>();
+        foreach (SerializedPair pair in _values)
+            newDict[pair.key] = pair.value;
+
+        return newDict;
     }
 }
