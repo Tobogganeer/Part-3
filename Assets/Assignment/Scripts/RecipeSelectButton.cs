1f498ec228be1e0315ba70669af9e4491f8de6c8
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-04-02 18:11:56 -0400

    Created RecipeSelectMenu UI

--- Assets/Assignment/Scripts/RecipeSelectButton.cs
+++ Assets/Assignment/Scripts/RecipeSelectButton.cs
@@ -2,21 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
 public class RecipeSelectButton : MonoBehaviour
 {
     // Store a recipe and tell our recipe menu when we are pressed
     public Recipe recipe;
+    public SpriteRenderer resultSprite;
 
-    SpriteRenderer spriteRenderer;
     RecipeSelectMenu menu;
 
     private void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
         menu = GetComponentInParent<RecipeSelectMenu>();
-
-        spriteRenderer.sprite = recipe.outputs[0].Sprite;
+        resultSprite.sprite = recipe.outputs[0].Sprite;
     }
 
     private void OnMouseDown()
